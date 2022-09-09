fe7be19ea7e080418c895dc90f26a3241116c551
Vcoder3 <victoryiserhienrhienn@gmail.com>
2022-09-08 18:08:24 -0700

    tried advance

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Return: one
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful"
+		- Dora Korpar, 2015-10-19",59);
+	return (1);
+}
